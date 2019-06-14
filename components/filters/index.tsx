27bfed16e25bcff1fc9bda36dac948dec24665cb
xianghuijin <xianghuijin@guazi.com>
2019-06-14 17:29:15 +0800

    init project

---
+++ components/filters/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface Filters {
+
+}
+
+const Filters: React.SFC<Filters> = props => (
+    <div>
+
+    </div>
+);
+
+export default Filters
---
+++ components/standard-table/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface StandardTable {
+
+}
+
+const StandardTable: React.SFC<StandardTable> = props => (
+    <div>
+
+    </div>
+);
+
+export default StandardTable
